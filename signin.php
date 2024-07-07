4e67c351e8def81f024b9c002a99ea94c3e56cb9
xzysst <140248944+xzysst@users.noreply.github.com>
2024-07-07 18:38:29 +0530

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,24 @@
+<?php
+include ('db.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            header('Location: index.html');
+        } else {
+            echo "<p class='error'>Invalid password, please try again</p>";
+        }
+    } else {
+        echo "<p class='error'>No user found with this email</p>";
+    }
+}
+
+$conn->close();
+?>
